c871d0c2037c0a8fed43880018f3b7065c3d2be9
Sel-Vin Kuik <selvinkuik@gmail.com>
2020-07-23 20:33:36 +0100

    With GR Racing logo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 var pckry = new Packery('.grid', {
   itemSelector: '.grid-item',
+  stamp: '.stamp',
   gutter: 8
 })
+
+var imgLoad = imagesLoaded('.grid', function() {
+  pckry.layout()
+})
+
+imgLoad.on('progress', function() {
+  pckry.layout()
+});
+
+
