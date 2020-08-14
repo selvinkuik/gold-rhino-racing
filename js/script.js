2cfc57e51cd9fab5806c3c31fff169151627e3e5
Sel-Vin Kuik <selvinkuik@gmail.com>
2020-08-14 21:22:17 +0100

    Desktop version

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
-var pckry = new Packery('.grid', {
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-item--sizer',
-  gutter: 20,
-  percentPosition: true
-})
+// var pckry = new Packery('.grid', {
+//   itemSelector: '.grid-item',
+//   columnWidth: '.grid-item--sizer',
+//   gutter: 20,
+//   percentPosition: true
+// })
 
-var imgLoad = imagesLoaded('.grid', function() {
-  pckry.layout()
-})
+// var imgLoad = imagesLoaded('.grid', function() {
+//   pckry.layout()
+// })
 
-imgLoad.on('progress', function() {
-  pckry.layout()
-});
+// imgLoad.on('progress', function() {
+//   pckry.layout()
+// })
 
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
 
+function layout() {
+  var scale = window.innerWidth / 1400,
+      x = (window.innerWidth - 1400) / 2,
+      y = (window.innerHeight - 1003.22) / 2,
+      transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  if (window.innerWidth > 1400) {
+    transform += ' scale(' + scale + ')'
+  }
+
+  document.querySelector('.grid').style.transform = transform
+}
+
+ready(layout)
+window.addEventListener('resize', layout)
